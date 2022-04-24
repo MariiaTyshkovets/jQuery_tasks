2b0c249bfc6ccf1d566274f9ecb52432c3a35ce6
Mariia Tyshkovets <maria15021996@gmail.com>
2022-04-24 19:47:08 +0300

    added slick-slider

--- script/index.js
+++ script/index.js
@@ -70,4 +70,15 @@ $(document).ready(() => {
         $(".window").html(remove());
         $(".result").empty();
     });
+
+    // Slick slider
+
+    $('.variable-width').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
 });
