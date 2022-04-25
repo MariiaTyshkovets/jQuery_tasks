b38de5740e9386c6173d23f219490067d4323304
Mariia Tyshkovets <maria15021996@gmail.com>
2022-04-25 22:17:21 +0300

    added ajax task

--- script/index.js
+++ script/index.js
@@ -7,6 +7,7 @@ $(document).ready(() => {
     
     // Калькулятор поки зроблений на одну дію, або якщо використовувати один той самий оператор декілька разів. Наприклад, 1+2, або 1*3*4.
     let text = "";
+    let lastBtnText = "";
     
     const calc = () => {
         let arr = [];
@@ -50,9 +51,15 @@ $(document).ready(() => {
         return text;
     }
 
+    const operator = (oper) => (oper == "*") || (oper == "+") || (oper == "-") || (oper =="/");
+
     $(".btn").click(function() {
-        $(".result").empty(); 
+        $(".result").empty();
+        if (operator(lastBtnText) && operator($(this).text())) {
+            $(".window").html(remove());
+        }
         $(".window").append(`${$(this).text()}`);
+        lastBtnText = $(this).text();
         text = $(".window").text(); 
     });
 
@@ -80,5 +87,54 @@ $(document).ready(() => {
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true
-      });
+    });
+
+    // AJAX
+
+    const API = 'https://jsonplaceholder.typicode.com';
+
+    fetch(`${API}/users`)
+    .then(response => response.json())
+    .then(users => {
+        const ajax = document.getElementsByClassName('ajax')[0];
+        const table = document.createElement('TABLE');
+        const userThemes = document.createElement('TR'); 
+        ajax.appendChild(table);
+        table.appendChild(userThemes);
+        userThemes.innerHTML = "<th>id</th>" + "<th>name</th>" + "<th>username</th>" + "<th>email</th>" + "<th>citi</th>" + "<th>phone</th>" + "<th>website</th>" + "<th>company</th>";
+        users.forEach(user => table.appendChild(getUsers(user)));
+    })
+    .catch(error => console.log(error));
+
+    function getUsers(user) {
+        const userPost = document.createElement('TR');
+        const tdId = document.createElement('TD');
+        const tdName = document.createElement('TD');
+        const tdUsername = document.createElement('TD');
+        const tdEmail = document.createElement('TD');
+        const tdCity = document.createElement('TD');
+        const tdPhone = document.createElement('TD');
+        const tdWebsite = document.createElement('TD');
+        const tdCompany = document.createElement('TD');
+
+        tdId.innerText = user.id;
+        tdName.innerText = user.name;
+        tdUsername.innerText = user.username;
+        tdEmail.innerText = user.email;
+        tdCity.innerText = user.address.city;
+        tdPhone.innerText = user.phone;
+        tdWebsite.innerText = user.website;
+        tdCompany.innerText = user.company.name;
+
+        userPost.appendChild(tdId);
+        userPost.appendChild(tdName);
+        userPost.appendChild(tdUsername);
+        userPost.appendChild(tdEmail);
+        userPost.appendChild(tdCity);
+        userPost.appendChild(tdPhone);
+        userPost.appendChild(tdWebsite);
+        userPost.appendChild(tdCompany);
+
+        return userPost;
+    }
 });
